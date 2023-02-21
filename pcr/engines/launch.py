323274162216f3dda74182e3fc6afd89bef1ca75
Xiaoyang Wu <gofinge.98@gmail.com>
2023-02-21 10:57:25 +0800

    Update launch.py

--- pcr/engines/launch.py
+++ pcr/engines/launch.py
@@ -86,59 +86,6 @@ def launch(
     else:
         main_func(*cfg)
 
-
-def hfai_launch(
-    main_func,
-    cfg=(),
-    timeout=DEFAULT_TIMEOUT,
-):
-    """
-    Launch multi-gpu or distributed training.
-    This function must be called on all machines involved in the training.
-    It will spawn child processes (defined by ``num_gpus_per_machine``) on each machine.
-    Args:
-        main_func: a function that will be called by `main_func(*args)`
-        timeout (timedelta): timeout of the distributed workers
-        args (tuple): arguments passed to main_func
-    """
-    ip = os.environ['MASTER_ADDR']
-    port = os.environ['MASTER_PORT']
-    num_machines = int(os.environ['WORLD_SIZE'])  # node number
-    machine_rank = int(os.environ['RANK'])  # node rank
-    num_gpus_per_machine = torch.cuda.device_count()  # num gpus for each node
-    dist_url = f"tcp://{ip}:{port}"
-
-    world_size = num_machines * num_gpus_per_machine
-
-    mp.spawn(
-        _distributed_worker,
-        nprocs=num_gpus_per_machine,
-        args=(
-            main_func,
-            world_size,
-            num_gpus_per_machine,
-            machine_rank,
-            dist_url,
-            cfg,
-            timeout,
-        ),
-        daemon=False,
-    )
-
-    if world_size > 1:
-        if dist_url == "auto":
-            assert num_machines == 1, "dist_url=auto not supported in multi-machine jobs."
-            port = _find_free_port()
-            dist_url = f"tcp://127.0.0.1:{port}"
-        if num_machines > 1 and dist_url.startswith("file://"):
-            logger = logging.getLogger(__name__)
-            logger.warning(
-                "file:// is not a reliable init_method in multi-machine jobs. Prefer tcp://"
-            )
-    else:
-        main_func(*cfg)
-
-
 def _distributed_worker(
     local_rank,
     main_func,
