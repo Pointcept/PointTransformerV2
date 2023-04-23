3eab6e341b1434b70122d3f23f2272ed8378f5a5
Xiaoyang Wu <gofinge.98@gmail.com>
2023-04-24 00:09:47 +0800

    Update preprocess_s3dis.py

--- pcr/datasets/preprocessing/s3dis/preprocess_s3dis.py
+++ pcr/datasets/preprocessing/s3dis/preprocess_s3dis.py
@@ -90,21 +90,21 @@ def main_process():
     room_list = []
 
     for i in range(1, 7):
-        if "Aligned_Version" in opt.source_root:
-            area_dir = os.path.join(opt.source_root, "Area_{}".format(i))
+        if "Aligned_Version" in opt.dataset_root:
+            area_dir = os.path.join(opt.dataset_root, "Area_{}".format(i))
             room_name_list = os.listdir(area_dir)
             room_name_list = [room_name for room_name in room_name_list
                               if (room_name != ".DS_Store" and ".txt" not in room_name)]
             room_list += [os.path.join("Area_{}".format(i), room_name) for room_name in room_name_list]
         else:
-            area_dir = os.path.join(opt.source_root, "Area_{}".format(i))
+            area_dir = os.path.join(opt.dataset_root, "Area_{}".format(i))
             align_dir = os.path.join(area_dir, "Area_{}_alignmentAngle.txt".format(i))
             room_name_list = np.loadtxt(align_dir, dtype=str)
             room_list += [[os.path.join("Area_{}".format(i), room_name[0]), int(room_name[1])]
                           for room_name in room_name_list]
 
     pool = mp.Pool(processes=mp.cpu_count())
-    pool.starmap(parse_room, [(room, opt.source_root, opt.save_root, opt.parse_normals)
+    pool.starmap(parse_room, [(room, opt.dataset_root, opt.save_root, opt.parse_normals)
                               for room in room_list])
     pool.close()
     pool.join()
